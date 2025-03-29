dbf020a6939b15c6f2b1d5b36404b9cb32cc61d3
Muskan <muskanbindal825@gmail.com>
2025-03-29 16:28:28 +0530

    Solution to Find the Pair with the Minimum Difference

--- Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -8,27 +8,32 @@ int main()
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
-    for(int j=n-1;j>=0;j--){
-        int i=0;
-        while(i!=j){
-            if(arr[i] > arr[j]){
-                int temp=arr[j];
-                arr[j]=arr[i];
-                arr[i]=temp;
+    if(n==1){
+        printf("%d",-1);
+    }
+    else{
+        for(int j=n-1;j>=0;j--){
+            int i=0;
+            while(i!=j){
+                if(arr[i] > arr[j]){
+                    int temp=arr[j];
+                    arr[j]=arr[i];
+                    arr[i]=temp;
+                }
+                i++;
             }
-            i++;
         }
-    }
-    int ans=INT_MAX,num1,num2;
-    for(int i=1;i<n;i++){
-        int diff=arr[i]-arr[i-1];
-        if(diff < ans){
-            ans=diff;
-            num1=arr[i-1];
-            num2=arr[i];
+        int ans=INT_MAX,num1,num2;
+        for(int i=1;i<n;i++){
+            int diff=arr[i]-arr[i-1];
+            if(diff < ans){
+                ans=diff;
+                num1=arr[i-1];
+                num2=arr[i];
+            }
         }
+        printf("%d %d",num1,num2);
     }
-    printf("%d %d",num1,num2);
     return 0;
 
 }
